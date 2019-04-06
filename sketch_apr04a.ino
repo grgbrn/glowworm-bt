b6e57c2b5a461c0b95d5dd313a61e929e4380628
Greg Brown <grgbrn@gmail.com>
2019-04-06 12:56:48 +0200

    crude class for independent animations

--- sketch_apr04a.ino
+++ sketch_apr04a.ino
@@ -9,6 +9,8 @@ extern uint32_t hsv2rgb(uint16_t h, uint8_t s, uint8_t v);
 #define PIXEL_COUNT 50
 #define PIN 2
 
+#define ANIMATION_SLOTS 4
+
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
 // Parameter 3 = pixel type flags, add together as needed:
@@ -24,12 +26,69 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIN, NEO_RGB + NEO_KHZ8
 // and minimize distance between Arduino and first pixel.  Avoid connecting
 // on a live circuit...if you must, connect GND first.
 
-//
-// show vars
-//
-uint32_t currentColor = strip.Color(0,0,255);
-uint8_t  currentIndex = 0;
-boolean  currentState = true;
+
+// simple breathe animation
+// http://sean.voisen.org/blog/2011/10/breathing-led-with-arduino/
+
+uint8_t breathe_intensity(unsigned long millis) {
+  float val = (exp(sin(millis/2000.0*PI)) - 0.36787944)*108.0;
+  return int(val);
+}
+
+class Animation
+{
+  int                 id;
+  Adafruit_NeoPixel *strip;
+
+  // reassigned on each new animation start
+  unsigned long   startTime;
+  boolean         active;
+
+  uint16_t        hue;
+  int             pixelIndex;
+  
+  public:
+  // XXX only necessary because we're using default ctor for static array simplicity
+  void init(int id, Adafruit_NeoPixel *s) {
+    this->id = id;
+    this->strip = s;    
+  }
+
+  void debug() {
+    Serial.print("* anim:");
+    Serial.print(this->id);
+    Serial.print(" hue:");
+    Serial.print(this->hue);
+    Serial.println();
+  }
+
+  void start(unsigned long ms, uint16_t hue, int pixelIndex) {
+    if (this->active) {
+      Serial.println("trying to start an already active show");
+      return;
+    }
+
+    this->active = true;
+    this->startTime = ms;
+    this->hue = hue;
+    this->pixelIndex = pixelIndex;
+  }
+
+  void tick(unsigned long ms) {
+    if (!this->active) return;
+    
+    uint8_t v = breathe_intensity(ms - this->startTime);
+    uint32_t currentColor = hsv2rgb(this->hue, 255, v);
+   
+    this->strip->setPixelColor(this->pixelIndex, currentColor);
+  }  
+};
+
+Animation anim[ANIMATION_SLOTS];
+uint16_t hues[] = {0,512,1024,1280};
+int tick = 0;
+int started = 0;
+
 
 void setup() {
   Serial.begin(115200);
@@ -38,39 +97,30 @@ void setup() {
   strip.begin();
   strip.setBrightness(80);
   strip.show(); // Initialize all pixels to 'off'
+  
+  for (int i=0; i<ANIMATION_SLOTS; i++) {
+    anim[i].init(i, &strip);
+  }
 }
 
 void loop() {
 
-  // loop around the color wheel, holding S & V constant
-  uint16_t h = 1280;
-  uint8_t v = 255; // start at full brightness
-  
-  while (true) {
-    // XXX need to brush up on my c idioms!
-//    h += 8;
-//    if (h >= 1536) {
-//      h = 0;
-//    }
+  unsigned long ms = millis();
 
-    v = breathe_intensity(millis());
+  if (tick++ % 500 == 0) {
+    if (started < ANIMATION_SLOTS) {
+      Serial.print("starting animation:");
+      Serial.println(started);
 
-    currentColor = hsv2rgb(h, 255, v);
-    Serial.println(v);
-
-    for (int j=0; j<PIXEL_COUNT; j++) {
-      strip.setPixelColor(j, currentColor);      
+      anim[started].start(ms, hues[started], started);
+      started++;
     }
-    strip.show();
-    
-    delay(10);
-  }  
-}
+  }
+  
+  for (int i=0; i<ANIMATION_SLOTS; i++) {
+    anim[i].tick(ms);
+  }
 
-// simple breathe animation
-// http://sean.voisen.org/blog/2011/10/breathing-led-with-arduino/
-
-uint8_t breathe_intensity(unsigned long millis) {
-  float val = (exp(sin(millis/2000.0*PI)) - 0.36787944)*108.0;
-  return int(val);
+  strip.show();
+  delay(10);
 }
